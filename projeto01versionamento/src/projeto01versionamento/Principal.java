cea5d6639485ac338accde9370cdc1f2be7ec39b
55119 <55119@DESKTOP-FMLFQCR>
2022-04-01 20:19:35 -0300

    adicionando uma classe

--- projeto01versionamento/src/projeto01versionamento/Principal.java
+++ projeto01versionamento/src/projeto01versionamento/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("ISSO É UM TESTE DE VERSIONAMENTO");
+		System.out.println("ISSO É UM TESTE DE VERSIONAMENTO");
+		System.out.println("ISSO É UM TESTE DE VERSIONAMENTO");
 	}
 
 }
